6f09187b90ba7a96332f087c5219e42fc50aa6a4
personkbr <personalshop@mail.ru>
2021-01-03 00:12:57 +0300

    three commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testC2
+{
+    class Program
+    {
+       static void recursWrite(int [] array, int count = 0)
+        {
+            if (array.Length > count)
+            {
+                Console.WriteLine(array[count]);
+                
+                recursWrite(array, ++count);
+            }
+          
+        }
+
+       
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 6, 7, 19 };
+            recursWrite(arr);
+            
+        }
+    }
+}
